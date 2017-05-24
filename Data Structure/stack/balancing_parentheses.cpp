c053576da7e14ef41ff36ffbc701b185f823ac46
Suryaveer <1998sveer@gmail.com>
2017-05-24 17:00:33 +0530

    Update balancing_parentheses.cpp

--- Data Structure/stack/balancing_parentheses.cpp
+++ Data Structure/stack/balancing_parentheses.cpp
@@ -1,35 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	string s;
-	cin>>s;
-	stack<char> st;
-	for(int i=0;i<s.size();i++){
-		if(s[i]=='{'||s[i]=='['||s[i]=='('){
-			st.push(s[i]);
-			// cout<<st.top();
-		}
-		else if(s[i]=='}'||s[i]==']'||s[i]==')'){
-			if(st.top()=='{'&&s[i]=='}'){
-				st.pop();
-			}
-			
-			else if(st.top()=='['&&s[i]==']'){
-				st.pop();
-			}
-			
-			else if(st.top()=='('&&s[i]==')'){
-				st.pop();
-			}
-			
-			else{
-				cout<<"Parentheses are not balanced!"<<endl;
-				return 0;
-				}
-		}
-	}
-	if(st.empty())
-	cout<<"Parentheses are balanced\n";
-	else
-		cout<<"Parentheses are not balanced!"<<endl;
-}
+    
+        string exp;
+        cin>>exp;
+        stack<char> st;
+        int len = exp.size();
+        int i;
+        for(i=0;i<len;i++){
+            if(exp[i]=='('||exp[i]=='{'||exp[i]=='['){
+                st.push(exp[i]);
+            }
+            else if(!st.empty()){
+                if(st.top()=='['&&exp[i]==']'){
+                    st.pop();
+                }
+                else if(st.top()=='{'&&exp[i]=='}'){
+                    st.pop();
+                }
+                else if(st.top()=='('&&exp[i]==')'){
+                    st.pop();
+                }
+                else{
+                    break;
+                }
+            }
+            else{
+                break;
+            }
+        }
+        if(st.empty()&&i==len){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        
+        
+        
+    }
